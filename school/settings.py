b24affcb02fece9b9fcb0380f7b737f1002c045e
Airo <72136641+Shakir022@users.noreply.github.com>
2023-12-09 19:26:32 +0600

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+5#9v^zs=-eqkv+e9corf92#(e^_uggo!!b=g9(^2(iwbx97vr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
